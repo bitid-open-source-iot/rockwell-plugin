24854c55fd4fab8a706e5acf4383ca8f414032e3
claytoncc <clayton@openiot.co.za>
2021-08-18 14:21:11 +0200

    updates

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const auth = require('./lib/auth');
 const express = require('express');
 
 try {
-    const KGateway = require('./lib/kGateway');
-    // const WebSocket = require('./lib/socket');
+    // const KGateway = require('./lib/kGateway');
+    const WebSocket = require('./lib/socket');
     // const RockwellMain = require('./lib/rockwellMain');
     const ErrorResponse = require('./lib/error-response');
     const ModbusMainController = require('./lib/modbusMainController');
